93e6ead17388805d0bcbd89c95d6e6e1bd51a68f
Artem <artkmedicamentum@gmail.com>
2024-02-20 21:38:20 +0200

    first commit

---
+++ Artem/example/ch1_alert.js
@@ -0,0 +1,6 @@
+prompt("it my comment");
+//document.write("Javascript say hello!");
+   //console.log("Javascript say hello!");
+   //console.info("Javascript say hello!");
+   //console.error("Javascript say hello!");
+   //console.warn("Javascript say helo");
